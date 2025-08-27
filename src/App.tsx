c584d55b3b18d1f381adc24e31cdf89d7cb68ded
Karine Débora Wagner <karinedwagner@gmail.com>
2025-08-27 16:09:19 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-200 text-center flex items-center justify-center">
+      <h1 className="text-5xl font-bold">
+        Configuração inicial OK! 🎉
+      </h1>
+    </div>
+  )
+}
+
+export default App
