091fd9470f06bbb958807f3d9ef5f8c8714f8104
Karine Débora Wagner <karinedwagner@gmail.com>
2025-09-04 11:50:51 -0400

    Add Footer component with developer information and year

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="flex justify-center items-center p-2 mt-2 sm:mt-4 lg:mt-6 lg:gap-6">
+      <span className="text-xs lg:text-xl">
+        Desenvolvido por Karine Wagner - 2025
+      </span>
+    </footer>
+  );
+}
