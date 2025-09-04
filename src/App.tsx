035516bf084867b21e21a11447b6e4653a20a116
Karine Débora Wagner <karinedwagner@gmail.com>
2025-09-04 11:51:35 -0400

    Refactor App component to implement routing with Header, Home, Details, NotFound, and Footer components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import './App.css';
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Header from "./components/layout/Header";
+import Home from "./pages/Home";
+import Details from "./pages/Details";
+import NotFound from "./pages/NotFound";
+import Footer from './components/layout/Footer';
+
 function App() {
   return (
-    <div className="min-h-screen bg-gray-200 text-center flex items-center justify-center">
-      <h1 className="text-5xl font-bold">
-        Configuração inicial OK! 🎉
-      </h1>
-    </div>
-  )
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
