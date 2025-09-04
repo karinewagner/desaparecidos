0c76a9baa6f9ed81fc63d8dc0156bf879409f8d5
Karine Débora Wagner <karinedwagner@gmail.com>
2025-09-04 17:04:59 -0400

    Add text color to Footer component for improved visibility

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="flex justify-center items-center p-2 mt-2 sm:mt-4 lg:mt-6 lg:gap-6">
+    <footer className="flex justify-center items-center text-white p-2 mt-2 sm:mt-4 lg:mt-6 lg:gap-6">
       <span className="text-xs lg:text-xl">
         Desenvolvido por Karine Wagner - 2025
       </span>
