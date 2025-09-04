a7eb459104bf8606eddaa000d0094ff7ee87679c
Karine Débora Wagner <karinedwagner@gmail.com>
2025-09-04 17:04:54 -0400

    Refactor Header component layout and update GitHub icon to improve UI consistency

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -28,50 +28,52 @@ export default function Header() {
 
   return (
   <>
-      <div className="flex justify-between items-center p-4 mb-2 sm:mb-4 lg:mb-6">
+      <div className="flex flex-col md:flex-row justify-around items-center gap-3 p-4 md:mb-2 lg:mb-6 
+      lg:w-10/12 mx-auto ">
 
-      <div className="flex flex-1 items-center">
-        <img
-          src="/assets/img/coat-of-arms.svg"
-          alt="Brasão da polícia civil"
-          className="h-12 sm:h-16 md:h-20 mr-3 cursor-pointer"
-          onClick={() => (window.location.href = "/")}
-        />
-        <span className="text-md sm:text-xl md:text-2xl lg:text-3xl font-semibold">
-          Pessoas Desaparecidas
-        </span>
-      </div>
-
-      <div className="hidden sm:flex flex-1 flex-col items-center justify-center gap-2">
-        <h2 className="p-1 sm:w-45 md:w-50 lg:w-65 text-center font-semibold rounded-xl">
-          LOCALIZADOS: {statistics.quantPessoasEncontradas}
-        </h2>
-        <h2 className="p-1 sm:w-45 md:w-50 lg:w-65 text-center font-semibold rounded-xl">
-          DESAPARECIDOS: {statistics.quantPessoasDesaparecidas}
-        </h2>
-      </div>
-
-      <div className="flex flex-1 flex-col items-end sm:gap-0.5 md:gap-1">
-        <h3 className="text-xs sm:text-xl md:text-2xl lg:text-3xl">
-          Karine Wagner
-        </h3>
-        <span className="text-[10px] sm:text-xs md:text-sm lg:text-lg">
-          Seletivo Desenvolve MT
-        </span>
-        <a
-          href="https://github.com/karinewagner/desaparecidos"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-[10px] sm:text-sm md:text-base lg:text-xl underline text-blue-400 flex gap-1 md:gap-2"
-        >
+        <div className="flex md:flex-1 items-center text-white ">
           <img
-            src="/assets/icons/github.png"
-            alt="Ícone do Github"
-            className="w-3 sm:w-4 md:w-5 lg:w-7 h-3 sm:h-4 md:h-5 lg:h-7"
+            src="/assets/img/coat-of-arms.svg"
+            alt="Brasão da polícia civil"
+            className="h-10 sm:h-12 md:h-16 mr-3 cursor-pointer"
+            onClick={() => (window.location.href = "/")}
           />
-          Repositório GitHub
-        </a>
-      </div>
+          <span className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-semibold">
+            Pessoas Desaparecidas
+          </span>
+        </div>
+
+        <div className="flex md:flex-1 flex-row md:flex-col items-center justify-center gap-2">
+          <h2 className="p-2 sm:w-45 md:w-50 lg:w-65 text-center font-semibold bg-blue-200 rounded-xl">
+            LOCALIZADOS: {statistics.quantPessoasEncontradas}
+          </h2>
+          <h2 className="p-2 sm:w-45 md:w-50 lg:w-65 text-center font-semibold bg-orange-300 rounded-xl">
+            DESAPARECIDOS: {statistics.quantPessoasDesaparecidas}
+          </h2>
+        </div>
+
+        <div className="hidden md:flex md:flex-1 flex-row md:flex-col md:items-center lg:items-end gap-2 text-white">
+          <h3 className="text-xs sm:text-xl md:text-2xl lg:text-3xl">
+            Karine Wagner
+          </h3>
+          <span className="text-[10px] sm:text-xs md:text-sm lg:text-lg">
+            Seletivo Desenvolve MT
+          </span>
+          <a
+            href="https://github.com/karinewagner/desaparecidos"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-[10px] sm:text-sm md:text-base lg:text-xl underline text-blue-400 flex gap-1 md:gap-2"
+          >
+            <img
+              src="/assets/icons/github.svg"
+              alt="Ícone do Github"
+              className="w-6 lg:w-7 h-6 lg:h-7"
+            />
+            Repositório GitHub
+          </a>
+        </div>
+
       </div>
 
       {alert && (
