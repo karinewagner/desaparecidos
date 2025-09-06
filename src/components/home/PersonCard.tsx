bb2cd1d71e9c997ea2247d94f630a04497c4b0a1
Karine Débora Wagner <karinedwagner@gmail.com>
2025-09-06 15:52:50 -0400

    Refactor PersonCard component to use Link for navigation and improve layout consistency

--- src/components/home/PersonCard.tsx
+++ src/components/home/PersonCard.tsx
@@ -8,7 +8,10 @@ interface Props {
 
 export default function PersonCard({ missingPerson }: Props) {
   return (
-    <div className="bg-white shadow-md rounded-md flex flex-col h-full">
+    <Link 
+      to={`/details/${missingPerson.id}`} 
+      className="bg-white shadow-md rounded-md flex flex-col h-full"
+    >
       <img
         src={
           missingPerson.urlFoto ||
@@ -18,40 +21,25 @@ export default function PersonCard({ missingPerson }: Props) {
         className="w-full h-60 object-cover rounded-t-md"
       />
 
-      <div className="flex-1 flex flex-col gap-2 mt-4 px-4">
-        <p className="font-semibold">
-          Nome: <span className="font-normal">{missingPerson.nome}</span>
-        </p>
-        <p className="font-semibold">
-          Idade: <span className="font-normal">{missingPerson.idade} anos</span>
-        </p>
-        <p className="font-semibold">
-          Sexo: <span className="font-normal">{missingPerson.sexo}</span>
-        </p>
-        <p className="font-semibold">
+      <div className="px-1 py-2 flex-col">
+        <p className="font-bold text-center text-sm lg:txt-base">{missingPerson.nome}</p>
+        <div className="text-center text-xs lg:text-sm">
+          <p>{missingPerson.sexo} - {missingPerson.idade} anos</p>
+        </div>
+        <hr className="my-2 border-t border-gray-300"></hr>
+        <p className="font-semibold text-center text-xs lg:text-sm">
           Data:{" "}
-          <span className="font-normal">
-            {new Date(missingPerson.ultimaOcorrencia.dtDesaparecimento).toLocaleDateString(
-              "pt-BR"
-            )}
+          <span className="font-normal text-xs lg:text-sm">
+            {missingPerson.ultimaOcorrencia.dtDesaparecimento}
           </span>
         </p>
-        <p className="font-semibold">
+          <p className="font-semibold text-center text-xs lg:text-sm">
           Local:{" "}
           <span className="font-normal">
             {missingPerson.ultimaOcorrencia.localDesaparecimentoConcat}
           </span>
         </p>
       </div>
-
-      <div className="mt-auto p-4">
-        <Link
-          to={`/details/${missingPerson.id}`}
-          className="w-full inline-block text-center bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700 transition"
-        >
-          Mais detalhes
-        </Link>
-      </div>
-    </div>
+    </Link>
   );
 }
