d1ab7e9161f6a27d76ba20299f65d6a9a8c50638
Karine Débora Wagner <karinedwagner@gmail.com>
2025-09-06 17:49:36 -0400

    update NotFound layout

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen text-center px-4">
+    <div className="-mt-50 flex flex-col items-center justify-center h-screen text-center px-4">
       <h1 className="text-6xl font-bold text-red-600">404</h1>
       <p className="text-xl mt-4">Página não encontrada</p>
       <p className="text-gray-500 mt-2">
@@ -10,7 +10,7 @@ export default function NotFound() {
       </p>
       <Link
         to="/"
-        className="mt-6 bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700 transition"
+        className="mt-6 bg-blue-800 text-white px-6 py-2 rounded hover:bg-blue-900 transition"
       >
         Voltar para a Home
       </Link>
