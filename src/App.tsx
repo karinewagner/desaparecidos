6f97bba8fddb26494f5e6ca30c7e64f6683d04a8
Karine Débora Wagner <karinedwagner@gmail.com>
2025-09-06 16:00:51 -0400

    Add BackToTop component to App for improved navigation

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,21 @@ import Header from "./components/layout/Header";
 import Home from "./pages/Home";
 import Details from "./pages/Details";
 import NotFound from "./pages/NotFound";
+import BackToTop from './components/common/BackToTop';
 import Footer from './components/layout/Footer';
 
 function App() {
   return (
     <Router>
       <Header />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/details/:id" element={<Details />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      
+      <BackToTop />
       <Footer />
     </Router>
   );
