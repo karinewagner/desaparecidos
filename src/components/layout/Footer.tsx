c88e72af590ab0467667b5016c49f3e80cbb55ed
Karine Débora Wagner <karinedwagner@gmail.com>
2025-09-06 17:54:30 -0400

    Update Footer Layout

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="relative bottom-0 flex justify-center items-center text-white p-4 mt-2 sm:mt-4 lg:mt-6 lg:gap-6 bg-gradient-to-r from-indigo-950/80 to-blue-700/70">
+    <footer className="relative bottom-0 flex justify-center gap-2 items-center text-white p-4 mt-2 sm:mt-4 lg:mt-6 lg:gap-6 bg-gradient-to-r from-indigo-950/80 to-blue-700/70">
       <span className="text-xs lg:text-xl">
         Desenvolvido por Karine Wagner - 2025
       </span>
